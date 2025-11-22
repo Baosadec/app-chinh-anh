6cdb225d7c95a61f9391f21b3cca5a116e46cf25
3Bao <tranbaosadec@gmail.com>
2025-11-22 22:06:35 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // QUAN TRỌNG: Thay đổi '/app-chinh-anh/' thành tên repository của bạn nếu khác
-  base: '/app-chinh-anh/', 
+  // Cấu hình đường dẫn cơ sở khớp với tên repository mới: chinh-anh-model
+  base: '/chinh-anh-model/', 
   plugins: [react()],
   define: {
-    // Gắn cứng API Key vào biến môi trường của ứng dụng
+    // Gắn cứng API Key vào biến môi trường của ứng dụng để chạy ngay lập tức
     'process.env.API_KEY': JSON.stringify("AIzaSyBkSrCmBaJK5ZPQpqCvZexTFZBfBbKyBK0"),
   },
 });
